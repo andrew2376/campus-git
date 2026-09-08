c1e3b5342c9433c96015b4d977fba4c2891ec855
kodevora <161730742+andrew2376@users.noreply.github.com>
2026-09-07 20:15:23 -0500

    Agregar nombre de la instancia

--- CampusStatus.java
+++ CampusStatus.java
@@ -1,5 +1,6 @@
 public class CampusStatus {
     public static void main(String[] args) {
         System.out.println("Campus Digital disponible");
+        System.out.println("Instancia: Backend-01");
     }
 }
